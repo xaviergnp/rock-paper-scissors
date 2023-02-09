3d6cb784b04e3e3c8c4e7deca2e02715b73ff1f9
xaviergnp <120711776+xaviergnp@users.noreply.github.com>
2023-02-09 23:19:29 +0800

    Change player win logic

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 "use strict";
-console.log("Rock Paper Scissors");
+console.log("Rock Paper Scissors Game");
 function getComputerChoice() {
     let randomNumber = Math.floor(Math.random()*3)+1;
     let computerChoice = randomNumber == 1? "rock" : randomNumber == 2 ? "paper" : "scissors";
@@ -7,27 +7,30 @@ function getComputerChoice() {
 }
 
 function playOneRound(playerSelection, computerSelection) {
-    if (String(playerSelection).length == String(computerSelection).length) {
-        return `It's a draw! ${playerSelection} v ${computerSelection}`;
-    } else if (String(playerSelection).length > String(computerSelection).length || 
-    (String(playerSelection).length == 4 && String(computerSelection).length == 8 )) {
-        return `You win! ${playerSelection} beats ${computerSelection}`;
+    let p1 = String(playerSelection).toUpperCase();
+    let comp1 = String(computerSelection).toUpperCase();
+    if (p1 == comp1) {
+        return `It's a draw! ${p1} v ${comp1}`;
+    } else if ((p1 == "SCISSORS" && comp1 == "PAPER") || (p1 == "PAPER" && comp1 == "ROCK") || (p1 == "ROCK" && comp1 == "SCISSORS")) {
+        return `You win! ${p1} beats ${comp1}`;
     } else {
-        return `You lose! ${playerSelection} is beaten by ${computerSelection}`;
+        return `You lose! ${p1} is beaten by ${comp1}`;
     }
 }
 
 function game() {
     // 5 rounds
     for (let x=0; x<5; x++) {
-        let round = `Rock Paper Scissors: Round ${x+1}`;
+        let round = `Rock Paper Scissors - Player vs AI - Round ${x+1}`;
         console.log(round);
         alert(round);
 
-        let playerSelection = prompt("Choose your hand: Rock | Paper | Scissors").toLowerCase();
+        let playerSelection = prompt("Choose your hand: Rock | Paper | Scissors");
         let computerSelection = getComputerChoice();
         let result = playOneRound(playerSelection, computerSelection);    
         console.log(result);
         alert(result);
     }
 }
+
+game();
