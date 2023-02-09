51e2291d6f6db882f6e61accba19a57d15383397
xaviergnp <120711776+xaviergnp@users.noreply.github.com>
2023-02-09 19:47:41 +0800

    Create computer choice function

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 "use strict";
 console.log("Rock Paper Scissors");
 function getComputerChoice() {
+    let randomNumber = Math.floor(Math.random()*30)+1;
+    let computerChoice = randomNumber == 1? "rock" : randomNumber == 2 ? "paper" : "scissors";
     return "Random: Rock Paper Scissors";
 }
 
@@ -11,6 +13,12 @@ function playOneRound(playerSelection, computerSelection) {
 function game() {
     // 5 rounds
     for (let x=0; x<5; x++) {
-        playOneRound();    
+        let round = `Rock Paper Scissors: Round ${x+1}`;
+        alert(round);
+        console.log(round);
+
+        let playerSelection = prompt("Choose your hand: Rock | Paper | Scissors");
+        let computerSelection = getComputerChoice();
+        playOneRound(playerSelection, computerSelection);    
     }
 }
