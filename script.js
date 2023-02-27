ee7e15af6d061ede422c98197b5d7d61683a1b5e
xaviergnp <120711776+xaviergnp@users.noreply.github.com>
2023-02-27 14:53:20 +0800

    Display result

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 "use strict";
-console.log("Rock Paper Scissors Game");
+// console.log("Rock Paper Scissors Game");
 function getComputerChoice() {
     let randomNumber = Math.floor(Math.random()*3)+1;
     let computerChoice = randomNumber == 1? "rock" : randomNumber == 2 ? "paper" : "scissors";
@@ -9,12 +9,13 @@ function getComputerChoice() {
 function playOneRound(playerSelection, computerSelection) {
     let p1 = String(playerSelection).toUpperCase();
     let comp1 = String(computerSelection).toUpperCase();
+    // Your Hand: ${p1}\nAI's Hand: ${comp1}\n\n
     if (p1 == comp1) {
-        return `Your Hand: ${p1}\nAI's Hand: ${comp1}\n\nIt's a draw! ${p1} v ${comp1}`;
+        return `It's a draw! ${p1} v ${comp1}`;
     } else if ((p1 == "SCISSORS" && comp1 == "PAPER") || (p1 == "PAPER" && comp1 == "ROCK") || (p1 == "ROCK" && comp1 == "SCISSORS")) {
-        return `Your Hand: ${p1}\nAI's Hand: ${comp1}\n\nYou WIN! ${p1} beats ${comp1}`;
+        return `You WIN! ${p1} beats ${comp1}`;
     } else {
-        return `Your Hand: ${p1}\nAI's Hand: ${comp1}\n\nYou LOSE! ${p1} is beaten by ${comp1}`;
+        return `You LOSE! ${p1} is beaten by ${comp1}`;
     }
 }
 
@@ -22,27 +23,31 @@ function getPlayerSelection() {
     const playerSelection = document.querySelectorAll(".selection > button");
     // let selectionMessage = document.querySelector(".message");
     let playerSelectedHand;
+    let resultList = document.querySelector(".result-list");
     playerSelection.forEach(select => {
         select.addEventListener("click", e => {
             playerSelectedHand = e.target.textContent;
             // selectionMessage.textContent = `Choose your hand: ${e.target.textContent}`;
 
             let computerSelection = getComputerChoice();
-            let result = playOneRound(playerSelectedHand, computerSelection);    
-            console.log(result);
-            alert(result);
+
+            const resultHolder = document.createElement("p");
+            resultList.appendChild(resultHolder);
+            resultHolder.textContent = playOneRound(playerSelectedHand, computerSelection);
+            // let result = playOneRound(playerSelectedHand, computerSelection);    
+            // console.log(result);
+            // alert(result);
         });
     });
 }
 
 function game() {
-        let x = 1;
-        let round = `Rock Paper Scissors - Player vs AI - Round ${x}`;
-        console.log(round);
-        alert(round);
+        // let x = 1;
+        // let round = `Rock Paper Scissors - Player vs AI - Round ${x}`;
+        // console.log(round);
+        // alert(round);
 
         getPlayerSelection();
-
 }
 
 game();
